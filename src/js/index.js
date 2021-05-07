7a501a2a368ac13a949944ca96cdc47b0eeecf4a
AgataS23 <agata.stoch@gmail.com>
2021-05-07 11:03:18 +0200

    feat: navigation.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 
 // uncomment the lines below to enable PWA
 // import {registerSW} from './pwa.js';
@@ -6,4 +6,5 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-console.log('HELLO I\'m Agata nice to meet you  :) 🚀')
+console.log("HELLO I'm Agata nice to meet you  :) 🚀");
+import "./navigation.js";
